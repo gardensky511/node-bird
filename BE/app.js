11d53bfacb3ed754f5c8761a42985390a488353e
luvmini511 <luvmini511@gmail.com>
2021-01-28 19:00:48 +0900

    express로 서버 생성 및 라우터 생성

---
+++ BE/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const postRouter = require('./routes/post')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello express')
+})
+
+app.get('/posts', (req, res) => {
+    res.json([
+        { id: 1, content: 'hello'},
+        { id: 2, content: 'hello'},
+        { id: 3, content: 'hello'},
+    ])
+})
+
+app.use('/post', postRouter)
+
+app.listen(3065, () => {
+    console.log('서버 실행중')
+})
+
+// app.get : 가져오다
+// app.post : 생성하다
+// app.delete : 제거
+// app.patch : 부분수정
+// app.put : 전체수정
+// app.options : 찔러보기(?)
+// app.head : 헤더만 가져오기
